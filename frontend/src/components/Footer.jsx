853edff755160344beeadd5a1519c9f83b03f94c
Ankita Sonawane <ankita.ganesh.sonawane505@gmail.com>
2025-12-09 21:32:45 +0530

    add all chnages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "../styles/Footer.css"
+import "../styles/Footer.css";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
