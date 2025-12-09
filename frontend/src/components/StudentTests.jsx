aa2e9269b10073d5d90b5581066255b3e9d3eda9
Ankita Sonawane <ankita.ganesh.sonawane505@gmail.com>
2025-12-09 21:41:21 +0530

    add all chnages

--- frontend/src/components/StudentTests.jsx
+++ frontend/src/components/StudentTests.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../api/axiosConfig";
-import "../styles/studentTests.css";
+import "../styles/StudentTests.css";
 
 const StudentTests = () => {
   const [tests, setTests] = useState([]);
