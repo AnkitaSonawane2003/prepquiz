3c8ad850d06851fbf2a7553bf08c5e9bca36a5e9
Ankita Sonawane <ankita.ganesh.sonawane505@gmail.com>
2025-12-10 11:08:18 +0530

    changes done

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -16,7 +16,8 @@ function isTokenExpired(token) {
 }
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+
   timeout: 20000,
 });
 
