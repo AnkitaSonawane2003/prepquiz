3d2cfa231638b5ca49e4ab32f00d5b7b964095db
Ankita Sonawane <ankita.ganesh.sonawane505@gmail.com>
2026-01-06 16:03:53 +0530

    Fix CORS and server config for backend-only deployment

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '@styles/Footer.css';
+import '@styles/footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
